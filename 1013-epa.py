7780f2ae105987e3b2d398ae8cc431cb08d51ee3
Xi-Plus <huangxuanyuxiplus@gmail.com>
2017-10-27 15:39:35 +0800

    update 1013-epa.py

--- 1013-epa.py
+++ 1013-epa.py
@@ -4,6 +4,7 @@ import json
 html = urlopen("https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259").read()
 datas = json.loads(html)
 requestCounty = ["南投縣", "高雄市"]
+print("SiteName", "PublishTime", "PM2.5", "AQI", "Status", "WindSpeed")
 for data in datas["result"]["records"]:
 	if data["County"] in requestCounty:
-		print(data["SiteName"], data["AQI"], data["PublishTime"])
+		print(data["SiteName"], data["PublishTime"], data["PM2.5"], data["AQI"], data["Status"], data["WindSpeed"])
