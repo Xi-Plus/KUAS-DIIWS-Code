0b89527a3459dcb4e381a83f4bc5a6631155f5c6
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-29 14:50:38 +0800

    update 1020-web2csv add set

--- 1020-web2csv.py
+++ 1020-web2csv.py
@@ -10,9 +10,12 @@ url = "http://ipv6.twbbs.org/Course/WebScraping/Slide/HTML/ex05_2_csv.html"
 html = urlopen(url).read()
 obj = BeautifulSoup(html, "html.parser")
 rows = obj.findAll("table")[0].findAll("tr")
+s = set()
 for row in rows:
 	res = []
 	for td in row.findAll(["td", "th"]):
 		res.append(td.get_text().strip())
-	w.writerow(res)
+	if res[0] not in s:
+		w.writerow(res)
+		s.add(res[0])
 f.close()
