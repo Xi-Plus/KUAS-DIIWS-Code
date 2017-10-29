a8dd8f3db26bb7b6465c0f6fd3418c876d48145f
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-29 14:38:18 +0800

    update 1027-urlretrieve

--- 1027-urlretrieve.py
+++ 1027-urlretrieve.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
+import os.path
 url = "http://kuas.ipv6.club.tw/~solomon/HTML/ch05_1_urlretrieve.html"
 f = urlopen(url)
 html = f.read()
@@ -7,5 +8,7 @@ obj = BeautifulSoup(html, "html.parser")
 imgs = obj.findAll("img")
 cnt = 0
 for img in imgs:
-	print(img["src"])
 	cnt += 1
+	imgurl = img["src"]
+	extension = os.path.splitext(imgurl)[1]
+	urlretrieve(imgurl, str(cnt)+extension)
